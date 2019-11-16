2a54fdbd98948274ae7f7bcd5327f3ddb180dcef
StéphaneDucasse <stephane.ducasse@inria.fr>
2019-11-16 15:47:26 +0100

    back to 2018

--- _support/latex/ensure-deps.sh
+++ _support/latex/ensure-deps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# this dependency list is for LaTex 2018
 
 xargs tlmgr install <<DEPS
     ctablestack
@@ -10,15 +11,17 @@ xargs tlmgr install <<DEPS
     gentium-tug
     graphics
     hyphen-english
+    ifetex
     import
     listings
+    luaotfload
     luatexbase
     memoir
+    ms
     multirow
     opensans
     pgf
     polyglossia
-    ragged2e
     tcolorbox
     tools
     trimspaces
